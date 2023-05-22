f7186bb147c82938ed939534ee9784c8d98ec3e9
Mhijazi16 <gultimate0@gmail.com>
2023-05-22 13:54:54 +0300

    Creation : create tblDoctor, ID as PK, DoctorName <= 40, firstEpisode <= LastEpisode

--- CreateDatabase.sql
+++ CreateDatabase.sql
@@ -12,3 +12,16 @@ CREATE TABLE tblAuthor
     AuthorID INT PRIMARY KEY, 
     AuthorName VARCHAR(40) CHECK (LEN(AuthorName) <= 40), 
 );
+
+CREATE TABLE tblDoctor
+(
+    DoctorID INT PRIMARY KEY, 
+    DoctorNumber INT NOT NULL, 
+    DoctorName VARCHAR(40), 
+    BirthDate DATE NOT NULL ,
+    LastEpisodeDate Date NOT NULL, 
+    FirstEpisodeDate Date NOT NULL, 
+
+    CHECK (FirstEpisodeDate <= LastEpisodeDate),
+    CHECK (LEN(DoctorName) <= 40)
+);
