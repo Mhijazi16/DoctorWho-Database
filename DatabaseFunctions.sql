71a506c8ce65769c1fcb25054f90e33d75224276
Mhijazi16 <gultimate0@gmail.com>
2023-05-22 18:10:24 +0300

    Functions : create fnCompanions function, returns list of companions for a given episode

--- DatabaseFunctions.sql
+++ DatabaseFunctions.sql
@@ -1,3 +1,20 @@
 USE DoctorWho
 
 GO 
+
+CREATE FUNCTION fnCompanions (@episodeID INT)
+RETURNS VARCHAR(MAX)
+BEGIN 
+    DECLARE @names VARCHAR(MAX)
+
+    SELECT @names = CONCAT_WS(', ' , @names, CompanionName)
+    FROM tblCompanion
+    WHERE tblCompanion.CompanionID  IN 
+    ( 
+        SELECT CompanionID FROM tblEpisodeCompanion AS ec
+        WHERE @episodeID = ec.EpisodeID 
+    );
+RETURN @names;
+END; 
+
+GO
