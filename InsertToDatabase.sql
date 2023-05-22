9a365fb35d4f31dba7e662fcae84c4e65f8ca305
Mhijazi16 <gultimate0@gmail.com>
2023-05-22 14:19:08 +0300

    Insertion : insert 5 rows to tblDoctor

--- InsertToDatabase.sql
+++ InsertToDatabase.sql
@@ -9,3 +9,10 @@ VALUES (1, 'Author 1'),
        (3, 'Author 3'),
        (4, 'Author 4'),
        (5, 'Author 5');
+
+INSERT INTO tblDoctor (DoctorID, DoctorNumber, DoctorName, BirthDate, LastEpisodeDate, FirstEpisodeDate)
+VALUES (1, 1001, 'Doctor 1', '1980-01-01', '2022-12-31', '1980-01-01'),
+       (2, 1002, 'Doctor 2', '1990-01-01', '2022-12-31', '1990-01-01'),
+       (3, 1003, 'Doctor 3', '2000-01-01', '2022-12-31', '2000-01-01'),
+       (4, 1004, 'Doctor 4', '2010-01-01', '2022-12-31', '2010-01-01'),
+       (5, 1005, 'Doctor 5', '2020-01-01', '2022-12-31', '2020-01-01');
