aee64b6502e939152021eb21859a542dd08fdc21
Mhijazi16 <gultimate0@gmail.com>
2023-05-22 14:46:31 +0300

    Modification : insert a record for episode with not doctors in it

--- ModifyDatabase.sql
+++ ModifyDatabase.sql
@@ -1,3 +1,6 @@
 USE DoctorWho 
 
 GO 
+
+INSERT INTO tblEpisode(EpisodeID, SeriesNumber, EpisodeNumber, EpisodeType, Title, EpisodeDate, AuthorID, DoctorID, Notes)
+        VALUES (15, 1, 4, 'Episode Type 1', 'Episode Title', '2022-01-01', 2, NULL, 'Notes 1');
