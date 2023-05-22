b9c46e65a42ae85ac7c5bd23390ef983dae358e1
Mhijazi16 <gultimate0@gmail.com>
2023-05-22 13:44:20 +0300

    Creation : create tblAuthor with ID as PK and put LEN <= 40 constraint on Name

--- CreateDatabase.sql
+++ CreateDatabase.sql
@@ -1,4 +1,14 @@
 
-CREATE DATABASE DocterWho
+CREATE DATABASE DoctorWho
 
 GO 
+
+USE DoctorWho
+
+GO 
+
+CREATE TABLE tblAuthor
+(
+    AuthorID INT PRIMARY KEY, 
+    AuthorName VARCHAR(40) CHECK (LEN(AuthorName) <= 40), 
+);
