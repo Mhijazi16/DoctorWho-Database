411ce6ae9fe8cd45dc5b1472ee357321a5af4873
Mhijazi16 <gultimate0@gmail.com>
2023-05-22 14:01:29 +0300

    Creation : create tblEpisode, ID as PK, AuthorID and DoctorID as FK, Title <= 35, Notes <= 255

--- CreateDatabase.sql
+++ CreateDatabase.sql
@@ -25,3 +25,21 @@ CREATE TABLE tblDoctor
     CHECK (FirstEpisodeDate <= LastEpisodeDate),
     CHECK (LEN(DoctorName) <= 40)
 );
+
+CREATE TABLE tblEpisode
+(
+    EpisodeID INT PRIMARY KEY, 
+    SeriesNumber INT NOT NULL,
+    EpisodeNumber SMALLINT NOT NULL, 
+    EpisodeType VARCHAR(30), 
+    Title VARCHAR(35) , 
+    EpisodeDate DATE NOT NULL, 
+    AuthorID INT, 
+    DoctorID INT, 
+    Notes NVARCHAR(255) , 
+
+    FOREIGN KEY (AuthorID) REFERENCES tblAuthor(AuthorID), 
+    FOREIGN KEY (DoctorID) REFERENCES tblDoctor(DoctorID), 
+    CHECK (LEN(Title) <= 35),
+    CHECK (LEN(Notes) <= 255)
+);
