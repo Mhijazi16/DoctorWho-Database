e058dfa0439b09af952c945ca4dc224a6f5e3a76
Mhijazi16 <gultimate0@gmail.com>
2023-05-22 17:14:31 +0300

    Modification : append -CANCELLED to title where DoctorID is Null and isn't already cancelled

--- ModifyDatabase.sql
+++ ModifyDatabase.sql
@@ -4,3 +4,6 @@ GO
 
 INSERT INTO tblEpisode(EpisodeID, SeriesNumber, EpisodeNumber, EpisodeType, Title, EpisodeDate, AuthorID, DoctorID, Notes)
         VALUES (15, 1, 4, 'Episode Type 1', 'Episode Title', '2022-01-01', 2, NULL, 'Notes 1');
+
+UPDATE tblEpisode SET Title = CONCAT(Title,'_CANCELLED')
+WHERE  DoctorID IS NULL AND Title NOT LIKE '%_CANCELLED'; 
