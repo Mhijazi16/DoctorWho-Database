bb274b540296c451b26c2a025c9d9a0ac1dd3ba3
Mhijazi16 <gultimate0@gmail.com>
2023-05-22 14:22:27 +0300

    Insertion : insert 5 rows to tblEpisode

--- InsertToDatabase.sql
+++ InsertToDatabase.sql
@@ -16,3 +16,11 @@ VALUES (1, 1001, 'Doctor 1', '1980-01-01', '2022-12-31', '1980-01-01'),
        (3, 1003, 'Doctor 3', '2000-01-01', '2022-12-31', '2000-01-01'),
        (4, 1004, 'Doctor 4', '2010-01-01', '2022-12-31', '2010-01-01'),
        (5, 1005, 'Doctor 5', '2020-01-01', '2022-12-31', '2020-01-01');
+
+INSERT INTO tblEpisode (EpisodeID, SeriesNumber, EpisodeNumber, EpisodeType, Title, EpisodeDate, AuthorID, DoctorID, Notes)
+VALUES (1, 1, 1, 'Episode Type 1', 'Episode Title 1', '2022-01-01', 1, 1, 'Notes 1'),
+       (2, 1, 2, 'Episode Type 2', 'Episode Title 2', '2022-01-02', 1, 2, 'Notes 2'),
+       (3, 2, 1, 'Episode Type 1', 'Episode Title 3', '2022-02-01', 3, 3, 'Notes 3'),
+       (4, 2, 2, 'Episode Type 2', 'Episode Title 4', '2022-02-02', 5, 2, 'Notes 4'),
+       (5, 3, 1, 'Episode Type 1', 'Episode Title 5', '2022-03-01', 5, 5, 'Notes 5');
+       
