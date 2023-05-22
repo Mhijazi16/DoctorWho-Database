310ac21cc7b5f95276937202478ae18554c0a436
Mhijazi16 <gultimate0@gmail.com>
2023-05-22 18:43:15 +0300

    Functions : add fnEnemies Function, takes Episode id and return, names of enimies in that episode

--- DatabaseFunctions.sql
+++ DatabaseFunctions.sql
@@ -18,3 +18,18 @@ RETURN @names;
 END; 
 
 GO
+
+CREATE FUNCTION fnEnemies (@episodeID INT)
+RETURNS VARCHAR(MAX)
+BEGIN
+    DECLARE @names VARCHAR(MAX); 
+    SELECT @names = CONCAT_WS(', ',@names, EnemyName)
+    FROM tblEnemy
+    WHERE EnemyID IN (
+        SELECT EnemyID FROM tblEpisodeEnemy
+        WHERE @episodeID = tblEpisodeEnemy.EpisodeID
+    )
+RETURN @names
+END;
+
+GO 
