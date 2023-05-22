b4fc0f36019caeb91fc3cee0d05493454c109f0f
Mhijazi16 <gultimate0@gmail.com>
2023-05-22 14:17:16 +0300

    Insertion : insert 5 rows to tblAuthor

--- InsertToDatabase.sql
+++ InsertToDatabase.sql
@@ -2,3 +2,10 @@
 USE DoctorWho 
 
 GO
+
+INSERT INTO tblAuthor (AuthorID, AuthorName)
+VALUES (1, 'Author 1'),
+       (2, 'Author 2'),
+       (3, 'Author 3'),
+       (4, 'Author 4'),
+       (5, 'Author 5');
